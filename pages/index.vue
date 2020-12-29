e8c60fa8acaa425d3c8ee380410e691fb5612329
Ushinji <siguma971@gmail.com>
2020-12-29 21:25:28 +0900

    Add shooting point using css animation.

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <div class="container">
     <div>
       <h1 class="title">shooting_star_simulator</h1>
+      <div class="start" />
     </div>
   </div>
 </template>
@@ -12,7 +13,7 @@ import Vue from 'vue'
 export default Vue.extend({})
 </script>
 
-<style>
+<style lang="scss">
 .container {
   margin: 0 auto;
   min-height: 100vh;
@@ -21,5 +22,22 @@ export default Vue.extend({})
   align-items: center;
   text-align: center;
   background-color: black;
+}
+
+.start {
+  background-color: snow;
+  height: 16px;
+  width: 16px;
+  position: fixed;
+  border-radius: 50%;
+  top: 10%;
+  animation: anim 1s linear infinite;
+}
+
+@keyframes anim {
+  100% {
+    color: transparent;
+    top: 50%;
+  }
 }
 </style>
